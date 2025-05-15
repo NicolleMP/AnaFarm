cdac247a50c51ae957494d192d0232e645052d63
Patrick Prestes <patrickprestes977@gmail.com>
2025-05-15 13:42:15 -0300

    feat: structure of the created page

--- src/view/register/RegisterView.vue
+++ src/view/register/RegisterView.vue
@@ -1,7 +1,38 @@
 <template>
-  <div>
-    <h1>Página de Cadastro</h1>
-    </div>
+    <div class="register-container">
+      <div class="register-wrapper">
+        <div class="form-register">
+            <div class="logo">
+               <img src="../../assets/logo.png" alt="Logo">
+               <h2>Nós Somos a Anafarm</h2>
+            </div>
+            <form>
+                <p>Por favor, crie sua conta</p>
+
+                <input type="text" placeholder="Nome completo">
+                <input type="email" placeholder="seumail@email.com">
+                <input type="password" placeholder="Crie uma senha segura">
+                <input type="password" placeholder="Repita sua senha">
+
+                <button type="submit">Registrar-se</button>
+
+                <div class="login">
+                    <p>Já possui conta?</p>
+                    <button type="button">Login</button>
+                </div> 
+            </form>
+        </div>
+   
+
+        <div class="info-register">
+           <h3>Somos mais do que apenas uma empresa</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua...
+         </p>
+       </div>
+      </div>
+   </div>
 </template>
 
 <script>
