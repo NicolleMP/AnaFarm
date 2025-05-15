9f08ac41e9b592b35196690ed3469b186476adb5
NicolleMP <nimendespoltosi46@gmail.com>
2025-05-15 16:21:33 -0300

    feat: Basic structure of the login screen with information fields and forms

---
+++ src/view/login/LoginView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="login-container">
+    <div class="first-content content">
+      <div class="first-column">
+        <img src="../../assets/logo.png" alt="Logo AnaFarm" />
+        <h2 class="title">Seja bem-vindo ao Anafarm</h2>
+        <p class="text">Faça seu login</p>
+        <form class="form">
+          <input type="text" placeholder="Nome" required />
+          <input type="password" placeholder="Senha" required />
+          <button class="btn" type="submit">Entrar</button>
+        </form>
+        <a href="/">
+          <p class="text">Esqueceu sua senha?</p>
+        </a>
+        <div class="noCount">
+          <p class="text">Não possui uma conta?</p>
+          <router-link to="/register" class="btn-count"
+            >Criar uma conta</router-link
+          >
+        </div>
+      </div>
+      <div class="second-column">
+        <div class="content-second">
+          <h2 class="title">Somos mais que uma empresa</h2>
+          <p class="text">
+            Na Anafarm, unimos cuidado, ciência e dedicação para oferecer
+            medicamentos manipulados personalizados para cada paciente. Nossa
+            missão é promover saúde e bem-estar com qualidade, responsabilidade
+            e atendimento humanizado. Aqui, cada fórmula é feita com carinho,
+            pensando em você.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
