acac5fb7575d1a2f1ce795e6d06f6056cfe1435f
Patrick Prestes <patrickprestes977@gmail.com>
2025-05-15 12:25:58 -0300

    create page register e routes

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import RegisterView from '../view/register/RegisterView.vue'; 
+
+const routes = [
+  {
+    path: '/register', 
+    name: 'Register', 
+    component: RegisterView 
+  },
+  
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
---
+++ src/view/register/RegisterView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Página de Cadastro</h1>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
+
