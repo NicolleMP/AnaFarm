3f5419343ed59213b7ee3826494d914331d127d8
NicolleMP <nimendespoltosi46@gmail.com>
2025-05-15 16:27:16 -0300

    feat: Complete page styling

--- src/view/login/LoginView.vue
+++ src/view/login/LoginView.vue
@@ -35,3 +35,124 @@
     </div>
   </div>
 </template>
+
+<style>
+body {
+  margin: 0;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+  font-family: "Poppins", sans-serif;
+}
+
+.login-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 900px;
+  height: 500px;
+  background-color: #424242;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 1);
+  border-radius: 10px;
+  overflow: hidden;
+}
+
+.first-content {
+  display: flex;
+  width: 100%;
+  height: 100%;
+}
+
+.first-column,
+.second-column {
+  flex: 1;
+  padding: 40px;
+}
+
+.first-column img {
+  width: 10rem;
+  padding: 0 6.5rem;
+}
+
+.first-column .title {
+  text-align: center;
+  font-size: 1rem;
+  font-weight: 500;
+  color: #fff;
+}
+
+.first-column .text {
+  font-size: 0.8rem;
+  font-weight: 500;
+  color: #fff;
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+}
+
+input {
+  padding: 12px;
+  font-size: 16px;
+  background-color: #424242;
+  border: 1px solid #ccc;
+  color: #ccc;
+  border-radius: 5px;
+}
+
+.btn {
+  cursor: pointer;
+  padding: 12px;
+  font-size: 16px;
+  background: linear-gradient(to right, #0cb7f2, #ff69b4);
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+}
+
+.noCount {
+  display: flex;
+  gap: 3.5rem;
+  align-items: center;
+}
+
+.btn-count {
+  cursor: pointer;
+  font-size: 15px;
+  font-weight: 100;
+  text-decoration: none;
+  padding: 10px;
+  background-color: transparent;
+  color: #fff;
+  border: 2px solid #0cb7f2;
+  border-radius: 10px;
+  font-weight: 700;
+}
+
+.btn-count:hover {
+  background-color: #0cb7f2;
+  color: #424242;
+}
+
+.second-column {
+  background: linear-gradient(to right, #0cb7f2, #ff69b4);
+  color: #fff;
+}
+
+.content-second {
+  padding: 7rem 0;
+}
+
+.content-second .title {
+  font-size: 1.3rem;
+  font-weight: 500;
+}
+
+.content-second .text {
+  font-size: 0.8rem;
+}
+</style>
