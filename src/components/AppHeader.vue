2e06c1d6bb25f33317807188bda961b690a7dfe1
Patrick Prestes <patrickprestes977@gmail.com>
2025-05-22 02:10:15 -0300

    feat: style header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -28,5 +28,78 @@
 </template>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.header {
+  width: 100%;
+  background-color: #0cb7f2;
+  padding: 20px 0;
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.15);
+}
+
+.header-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.nav {
+  display: flex;
+  align-items: center;
+  gap: 1.6875rem; 
+  margin-right: 10rem; 
+}
+
+.logo img {
+  height: 3.75rem; 
+  transition: transform 0.3s ease;
+  margin-left: 6.25rem; 
+}
+
+.logo img:hover {
+  transform: scale(1.1);
+}
+
+.nav ul {
+  list-style: none;
+  display: flex;
+  gap: 2.1875rem; 
+  margin: 0;
+  padding: 0;
+}
+
+.nav a {
+  color: #fff;
+  text-decoration: none;
+  font-weight: 600;
+  position: relative;
+  transition: color 0.3s ease;
+}
+
+.nav a::after {
+  content: "";
+  position: absolute;
+  width: 0%;
+  height: 0.125rem; 
+  bottom: -0.3125rem; 
+  left: 0;
+  background-color: #fff;
+  transition: width 0.3s ease;
+}
+
+.nav a:hover::after {
+  width: 100%;
+}
+
+.user-icon {
+  color: #fff;
+  font-size: 1.5rem;
+  cursor: pointer;
+  transition: transform 0.3s ease;
+}
 
 </style>
