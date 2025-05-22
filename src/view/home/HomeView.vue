158f06a85275710fd718c222396d13130f5b240a
Patrick Prestes <patrickprestes977@gmail.com>
2025-05-22 03:07:13 -0300

    feat: add banner

--- src/view/home/HomeView.vue
+++ src/view/home/HomeView.vue
@@ -11,10 +11,37 @@ export default {
 <template>
   <div>
     <AppHeader/>
+    <section class="banner">
+      <img src="../../assets/bannerHome.png" alt="Banner Anafarm" /> 
+    </section>
+
     </div>
 </template>
 
-
 <style>
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Poppins', sans-serif;
+}
+ .banner {
+  width: 100%;
+  height: 33rem; 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden; 
+
+}
+
+.banner img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover; 
+  
+}
+
 
 </style>
