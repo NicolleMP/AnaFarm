680dab2eecca46c300c6b5ff3cb9bcb8dbcd1165
Patrick Prestes <patrickprestes977@gmail.com>
2025-05-22 03:12:59 -0300

    feat: create cta user

--- src/view/home/HomeView.vue
+++ src/view/home/HomeView.vue
@@ -15,6 +15,12 @@ export default {
       <img src="../../assets/bannerHome.png" alt="Banner Anafarm" /> 
     </section>
 
+     <section class="chamada-cta">
+      <h2>Transforme sua rotina com nossos produtos exclusivos!</h2>
+      <p>Descubra ofertas incríveis que só a Anafarm oferece para você cuidar da sua saúde com qualidade e economia.</p>
+      <button >Quero ver os produtos</button>
+    </section>
+
     </div>
 </template>
 
@@ -43,5 +49,47 @@ export default {
   
 }
 
+.chamada-cta {
+width: 100%;
+padding: 3rem 2rem;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+text-align: center;
+background-color: #f9f9f9;
+}
+
+.chamada-cta h2 {
+font-size: 2.5rem;
+font-weight: 700;
+margin-bottom: 1rem;
+color: #333; 
+}
+
+.chamada-cta p {
+font-size: 1.1rem;
+color: #555;
+margin-bottom: 2rem;
+line-height: 1.6;
+max-width: 700px; 
+}
+
+.chamada-cta button {
+background-color: #0cb7f2;
+color: white;
+font-weight: 600;
+font-size: 1.2rem;
+padding: 1rem 2.5rem;
+border: none;
+border-radius: 5px;
+cursor: pointer;
+box-shadow: 0 5px 15px rgba(0, 0, 0, 0.1);
+transition: background-color 0.3s ease;
+}
+
+.chamada-cta button:hover {
+background-color: #0a9bcf; 
+}
 
 </style>
